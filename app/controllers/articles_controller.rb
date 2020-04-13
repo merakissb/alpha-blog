aefe5526d21cf43251eab03f0fefbf3a91eab4c1
merakissb <mbustosquiroz@gmail.com>
2020-04-13 14:50:23 -0400

    se implemento la accion editar en el form,se agrego la ruta edit y update

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -9,4 +9,32 @@ class ArticlesController < ApplicationController
         @article = Article.all
     end
 
+    def new
+        @article = Article.new
+    end
+
+    def edit
+        @article = Article.find(params[:id])
+    end
+
+    def create
+        @article = Article.new(params.require(:article).permit(:title, :description))
+        if @article.save
+          flash[:notice] = "Articulo creado exitosamente."
+          redirect_to @article
+        else 
+          render 'new'
+        end
+    end
+
+    def update
+        @article = Article.find(params[:id])
+        if @article.update(params.require(:article).permit(:title, :description))
+        flash[:notice] = "Articulo actualizado exitosamente."
+        redirect_to @article
+        else
+          render 'edit'
+        end
+    end
+    
 end
