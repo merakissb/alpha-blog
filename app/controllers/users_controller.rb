d602754cda9901b4fa70610ed3ce55c6defcc11b
merakissb <mbustosquiroz@gmail.com>
2020-04-15 13:18:17 -0400

    Añadi la accion create al users_controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,5 +2,20 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
-end
 
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:notice] = "Welcome #{@user.username} to Reblog, tu sesion inicio correctamente."
+      redirect_to articles_path
+    else 
+      render 'new'
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:username, :email, :password)
+  end
+
+end
