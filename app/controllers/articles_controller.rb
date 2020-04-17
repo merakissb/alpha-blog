72ffda17d0e9957f6ced8967904a20901107403e
merakissb <mbustosquiroz@gmail.com>
2020-04-17 12:41:21 -0400

    Modifique la accion index del controlador de articulos para mostrar los articulos creados recientes al comienzo de la pagina.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -8,7 +8,7 @@ class ArticlesController < ApplicationController
     end
 
     def index
-        @articles = Article.paginate(page: params[:page], per_page: 5)
+        @articles = Article.order(created_at: :desc).paginate(page: params[:page], per_page: 5)
     end
 
     def new
