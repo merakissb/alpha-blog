8c026d238e060839576c78e6d2002ca28791f38d
merakissb <mbustosquiroz@gmail.com>
2020-04-30 08:53:55 -0400

    add meta in layouts/app and generate migration for active storage

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   before_save { self.email = email.downcase }
   has_many :articles
+  has_one_attached :avatar
   
   validates :username, presence: true, uniqueness: { case_sensitive: false }, length: { minimum: 3, maximum: 20}
   
