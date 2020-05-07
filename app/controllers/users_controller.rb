1e2cba30f494344d1decbde21f4e738e5d2a027a
merakissb <mbustosquiroz@gmail.com>
2020-05-07 09:43:49 -0400

    - Added functionality for users to delete their own accounts using a destroy method in the users controller.
    - Added a link to delete account to the users' profile dropdown navigation option.
    
    - Utilized a couple of existing methods (as before_action methods) to secure the newly created destroy action at controller level.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [:edit, :update, :show]
-  before_action :require_same_user, only: [:edit, :update]
+  before_action :set_user, only: [:edit, :update, :show, :destroy]
+  before_action :require_same_user, only: [:edit, :update, :destroy]
   
   def show
     @articles = @user.articles.order(created_at: :desc).paginate(page: params[:page], per_page: 3)
@@ -35,6 +35,13 @@ class UsersController < ApplicationController
     else 
       render 'new'
     end
+  end
+
+  def destroy
+    @user.destroy
+    session[:user_id] = nil
+    flash[:notice] = "Account and all associated articles successfully deleted"
+    redirect_to articles_path
   end
 
   private
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   before_save { self.email = email.downcase }
-  has_many :articles
+  has_many :articles, dependent: :destroy
   has_one_attached :avatar
   
   validates :username, presence: true, uniqueness: { case_sensitive: false }, length: { minimum: 3, maximum: 20}
