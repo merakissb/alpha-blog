014a50aa702fcba72f41a2bcef2fbc3f699c8183
merakissb <mbustosquiroz@gmail.com>
2020-06-16 09:25:53 -0400

    - Add controller test to ensure categories cannot be created by non-admins.
    - Add require_admin method (private) to categories controller and use it as before_action for new and create actions.
    
    - Create admin user in categories controller test and sign the user in for new and create action tests.
    
    - Build sign_in_as helper method to test_helper.rb file, so it can be used to sign users in for tests.
    
    - Modify categories integration test file to create and sign a user in (as admin) in order to proceed with the tests.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_07_134746) do
+ActiveRecord::Schema.define(version: 2020_06_01_200855) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -39,6 +39,12 @@ ActiveRecord::Schema.define(version: 2020_05_07_134746) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer "user_id"
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "users", force: :cascade do |t|
