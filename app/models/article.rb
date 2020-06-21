ee1a4f6bc620fdf220c5ba16313990de25212ce8
merakissb <mbustosquiroz@gmail.com>
2020-06-21 17:02:16 -0400

    changes in comment form and article form an show, validates comment and article avatar presence

--- app/models/article.rb
+++ app/models/article.rb
@@ -6,6 +6,8 @@ class Article < ApplicationRecord
     validates :title, presence: true, length: { minimum: 6, maximum: 100}
     validates :description, presence: true, length: { minimum: 16, maximum: 2000}
     validates :avatar, presence: true
+    validates :categories, presence: true
+    
     has_one_attached :avatar
     has_many :comments, dependent: :destroy
 end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,5 +2,5 @@ class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :article
 
-  validates :body, presence: true, allow_blank: false
+  validates :body, presence: true, allow_blank: false, length: { minimum: 4, maximum: 155}
 end
